a58f83e33066a4bf3093959721e15d1d803a247e
doaads <nowobart@gmail.com>
2025-04-14 19:49:32 +0200

    feat: better script readability

--- haskell/haskellize.sh
+++ haskell/haskellize.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-RESULT=$(cat ascii.pl | sed -s s/.*typewriter_write_text\(\'\\\\e/putStrLn\ \'\\\\x1b/)
+PROLOG_PREDICATE_TO_REPLACE="typewriter_write_text"
+
+RESULT=$(cat ascii.pl | sed -s s/.*$PROLOG_PREDICATE_TO_REPLACE\(\'\\\\e/putStrLn\ \'\\\\x1b/)
 RESULT=$(echo "$RESULT" | sed -s s/\\\\e/\\\\x1b/g)
 RESULT=$(echo "$RESULT" | sed -s s/\).*//)
 RESULT=$(echo "$RESULT" | sed -s s/\'/\"/g)
